67dd1fc3cdb03f5e74779bca35c1d54e6383fcc3
beomq <beomdolq@gmail.com>
2023-06-12 14:24:34 +0900

    033.공사중 해결 SingleChildScrollView

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -17,74 +17,76 @@ class _MainPageState extends State<MainPage> {
         title: Text('홈'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 30,
-            ),
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                 color: Colors.red,
-                fontSize: 70,
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('ElevatedButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OutlinedButton'),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자',
-                border: OutlineInputBorder(),
+              SizedBox(
+                height: 100,
               ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            Image.network(
-              'https://cdn.pixabay.com/photo/2023/05/31/17/54/cat-8031947_1280.jpg',
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-            Container(
-              color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/pic.jpg',
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+              Container(
+                height: 30,
+              ),
+              Text(
+                '숫자',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
                 ),
               ),
-            ),
-          ],
+              Text(
+                '$count',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 70,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('ElevatedButton'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('TextButton'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('OutlinedButton'),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              Image.network(
+                'https://cdn.pixabay.com/photo/2023/05/31/17/54/cat-8031947_1280.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/pic.jpg',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
